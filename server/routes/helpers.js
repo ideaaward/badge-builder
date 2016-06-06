8f0c0882189c8ff4418090d8be929f960ba8dbbf
Lilian Kasem <liliankasem@gmail.com>
2016-06-06 13:10:37 +0100

    Adding scoring logic for text quiz elements
    Not finished, still need to fix

--- server/routes/helpers.js
+++ server/routes/helpers.js
@@ -16,12 +16,41 @@ module.exports.calculateResults = function (badge, answers) {
   var results = {};
   badge.content.elements.forEach(function (element) {
     var id = element._id;
-    if (typeof element.answer !== 'undefined') {
-      results[id] = answers[id] + '' === element.answer + '';
+
+    if (typeof element.answer !== 'undefined') {         
+      switch(element.elementType){
+        case "yes-no":
+        case "multiple-choice":
+            results[id] = answers[id] + '' === element.answer + '';
+            break;
+        case "quiz-short-input":
+            var ans = element.answer;
+            var score = 0;
+            
+            element.answerKeywords.forEach(function(word) {
+              if(ans.indexOf(word) > -1){
+                score++;
+              }              
+            }, this);
+            
+            if(score >= element.answerKeywords.length){
+              results[id] = answers[id] + 'true';
+            }else{
+              esults[id] = answers[id] + 'false';
+            }
+            
+            break;
+        case "quiz-long-input":
+            // TODO
+            break;
+        default:
+            break;      
+      }     
     } else {
       // TODO: Handle content elements without answers better.
       results[id] = true;
     }
-  });
+        
+  });    
   return results;
 };
