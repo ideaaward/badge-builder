d68a1f597785b639ebf523f473eaaed28289b0dc
Ville Rantala <ville.rantala@microsoft.com>
2016-06-20 12:24:43 +0300

    Change deployment target

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,12 @@ set -o pipefail
 
 if [ "$TRAVIS_BRANCH" = "master" ]
 then
+  git config --global user.name "Travis CI"
+  git config --global user.email "noreply@travis-ci.org"
   cd dist
   git init .
   git add *
-  git commit -m "Deployment commit" --author="Travis CI <noreply@travis-ci.org>"
+  git commit -m "Deployment commit"
   # We redirect any output to /dev/null to hide
   # any sensitive credential data that might otherwise be exposed.
   git push --force --quiet "https://${GIT_USER}:${GIT_PASSWORD}@${GIT_TARGET}" master:master > /dev/null 2>&1
