ffc92cfd740f922ec62aa8daa95b27650e7513b8
Ville Rantala <ville.rantala@microsoft.com>
2016-06-22 10:59:11 +0300

    Implement author management
    Fixes #107.

--- server/routes/api.js
+++ server/routes/api.js
@@ -243,16 +243,20 @@ router.get('/admin/admins', requireAdmin, function (req, res) {
   getUsers('admin', req, res);
 });
 
+router.get('/admin/authors', requireAdmin, function (req, res) {
+  getUsers('author', req, res);
+});
+
 router.get('/admin/users', requireAdmin, function (req, res) {
   getUsers('user', req, res);
 });
 
-router.put('/admin/admins', requireAdmin, function (req, res) {
+var changeRole = function (req, res, role) {
   models.User.findOne({ id: req.body.id }, function (err, user) {
     if (err || user === null) {
       return sendBadRequest(res);
     }
-    user.role = 'admin';
+    user.role = role;
     user.save(function (err) {
       if (err) {
         return sendError(res, err);
@@ -262,6 +266,14 @@ router.put('/admin/admins', requireAdmin, function (req, res) {
       });
     });
   });
+};
+
+router.put('/admin/admins', requireAdmin, function (req, res) {
+  changeRole(req, res, 'admin');
+});
+
+router.put('/admin/authors', requireAdmin, function (req, res) {
+  changeRole(req, res, 'author');
 });
 
 module.exports = router;
