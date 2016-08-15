97ed28aea80a726b9a38a952fac8ab6f6f47d8a7
Lilian Kasem <liliankasem@gmail.com>
2016-08-15 16:03:05 +0100

    Changed error text

--- server/errors.js
+++ server/errors.js
@@ -5,7 +5,7 @@ var errors = {
   USER_NOT_IDEA_USER: 'You must first register in the iDEA hub.',
   AUTHENTICATION_FAILURE: 'Authentication failure.',
   DATABASE_ERROR_SAVE: 'Error saving to database.',
-  USER_UNAUTHORIZED: 'You are not authorized to use Badge Builder. Please contact the iDEA team by emailing to XYZ to get instructions on how to get started.'
+  USER_UNAUTHORIZED: 'You are not authorized to use Badge Builder. Please contact the iDEA team for instructions on how to get started.'
 };
 
 module.exports = errors;
